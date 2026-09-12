b5768cade803417dcd539ee8b60c6222fa613213
timelessmineralcreations <timelessmineralcreations@gmail.com>
2026-09-11 23:15:54 -0400

    Add Google Analytics tracking

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,8 @@ import {
   Geist_Mono,
 } from "next/font/google";
 
+import Script from "next/script";
+
 import "./globals.css";
 
 import Header from "@/components/layout/Header";
@@ -64,6 +66,22 @@ export default async function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-61349C6EBT"
+          strategy="afterInteractive"
+        />
+
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+        >
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-61349C6EBT');
+          `}
+        </Script>
         <CartProvider>
           {showAnnouncement && (
             <div
